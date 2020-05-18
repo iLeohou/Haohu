c28a72a50738e0b80b620c166910d6f5a391e8d7
ranhaoliu(刘然浩) <ranhaoliu@tencent.com>
2020-05-18 14:34:16 +0800

    实现next链式调用，并增加错误处理

--- test/index.js
+++ test/index.js
@@ -1,9 +1,14 @@
 const Haohu = require('../index');
 const app = Haohu();
 
-app.get('/', (req, res) => {
-    res.end("667sss")
+app.get('/', (req, res, next) => {
+    next();
 })
+.get('/', (req, res, next) => {
+    res.send("666")
+})
+
+
 
 app.listen(3000, () => {
     console.log("listening 3000")
